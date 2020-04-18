6e42efb1df41990bda46563e844fcf9aff57b2b2
Richard Hancock <me@richardhancock.co.uk>
2020-04-18 17:34:55 +0100

    Included missing files left out of previous commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 5.0f;
+
+    private Rigidbody rb;
+
+    private GameObject heldItem = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+        rb.AddForce(movement * movementSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
