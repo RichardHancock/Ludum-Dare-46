53a0ff7ccb1823078480700bdf6457b7b2ca0ad5
Richard Hancock <me@richardhancock.co.uk>
2020-04-19 16:35:38 +0100

    More work on Rack and Module Classes No testing yet

--- Assets/Script/Rack.cs
+++ Assets/Script/Rack.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Rack : Interactable
+public abstract class Rack : Interactable
 {
     public int MaxCapacity { get; protected set; }
-    protected List<RackModule> Modules;
+    protected List<GameObject> Modules;
 
     protected float Health;
 
@@ -20,6 +20,15 @@ public class Rack : Interactable
 
     void Update()
     {
+        if (Modules.Count > 0)
+        {
+            Health -= (0.05f * (float)Modules.Count) * Time.deltaTime;
+        }
+
+        if (Health < 50.0f)
+        {
+            Debug.Log("Low Health");
+        }
         
     }
 
@@ -32,25 +41,44 @@ public class Rack : Interactable
 
         RackModule module = item.GetComponent<RackModule>();
         
-        if (module == null || module.Type == CompatibleType)
+        //Check if compatible with this rack type
+        if (module == null || module.Type != CompatibleType)
         {
             Debug.Log("This Module is Incompatible with This Rack");
         }
 
-
-        //Check if module compatible if so add to server
-        return false;
+        //Add To Rack
+        Modules.Add(item);
+        return true;
     }
 
     public override bool Interact()
     {
+        //TODO: Implement interact on server to resolve a failure or dust it
         throw new System.NotImplementedException();
     }
 
     public virtual int GetTotalOutput()
     {
         //Iterate through all rack modules and total their performance, then subtract any modifiers (Dustiness)
+        //TODO: Implement once rack module is implemented
+        int output = 0;
 
-        return 0;
+        foreach (GameObject obj in Modules)
+        {
+            output += obj.GetComponent<RackModule>().GetOutput();
+        }
+
+        return output;
     }
+
+    //Check if the rack should experience a local failure, if so trigger a local failure event
+    public virtual bool LocalFailCheck()
+    {
+        //TODO Switch to check module failure instead
+        TriggerLocalFailure();
+        return true;
+    }
+
+    protected abstract void TriggerLocalFailure();
 }
--- Assets/Script/RackModule.cs
+++ Assets/Script/RackModule.cs
@@ -13,15 +13,70 @@ public class RackModule : Pickup
 
     public ModuleType Type { get; protected set; }
 
+    protected int RawOutput;
+
+    protected int Age = 0;
+
+    private bool Active = false;
+
+    public int FailureThresholdMin;
+    public int FailureThresholdMax;
+    public int FailurePoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+           
+    }
+
+    public void ActivateModule()
+    {
+        if (Active)
+            return;
+
+        InvokeRepeating("AgeModule", 1.0f, 10.0f);
+    }
+
+    public void ResetModule()
+    {
+        Active = false;
+        Age = 0;
+    }
+
+    private void AgeModule()
+    {
+        Age += 1;
+    }
+
+    protected bool LocalFailCheck()
+    {
+        if (!Active)
+            return false;
+
+        if ((Random.Range(FailureThresholdMin, FailureThresholdMax) + Age) > FailurePoint)
+        {
+            //TODO Display Failure
+            Debug.Log(gameObject.name + "Failed");
+            CancelInvoke();
+            Active = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    public virtual int GetOutput()
+    {
+        //TODO (Maybe Done)have output get affected by failures
+        if (!Active)
+            return 0;
+
+        return RawOutput;
     }
 }
